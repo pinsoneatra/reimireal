56f419dcafc58e42d23fc6c5af0d58fba8b2820e
reimireal <pinsoneatra90@gmail.com>
2017-03-31 16:51:07 +0700

    fixed models class

--- app/init.php
+++ app/init.php
@@ -2,7 +2,6 @@
 require_once 'core/app.php';
 require_once 'core/controller.php';
 require_once 'config/config.php';
-require_once 'config/database.php';
 require_once 'vendor/autoload.php';
 //
 ?>
